dd40d6d9ca366e1b3855ca9a57f87317d206a83c
Nizar Nasri <nnasri@integrationobjects.com>
2024-11-22 13:53:29 +0100

    Add TennisPlayerManager Controller

---
+++ PlayersManager/Controllers/TennisPlayerManagerController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TennisPlayerApplication.DTOs;
+using TennisPlayerApplication.Interfaces;
+
+namespace PlayersManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TennisPlayerManagerController : ControllerBase
+    {
+
+        #region Attributes
+        private IPlayerService _playerService;
+
+        #endregion
+
+        #region Constructor
+        public TennisPlayerManagerController(IPlayerService playerService)
+        {
+            _playerService = playerService;
+        }
+
+        #endregion
+
+
+      
+
+
+
+
+
+
+
+    }
+}
